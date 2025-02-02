f7bd60eebd619e015d088e2860cd6822533646a9
Kundan <Kumar.kundan@Jeevansathi.com>
2025-02-02 15:35:09 +0530

    -- initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js - Interactive Animations
+
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.querySelector(".btn");
+    button.addEventListener("click", () => {
+        alert("Welcome to Money Genie!");
+    });
+});
